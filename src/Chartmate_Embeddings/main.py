492d10962027641d340a9837727e2f7439715e8e
jyotirmayee <156336518+jyotirmayee23@users.noreply.github.com>
2024-10-22 21:22:17 +0530

    ssm parameter added

--- src/Chartmate_Embeddings/main.py
+++ src/Chartmate_Embeddings/main.py
@@ -1,25 +1,20 @@
 import boto3
-from io import BytesIO
 import json
-import datetime
 import os
-import tempfile
-from botocore.config import Config
-import requests
-import io
+import time
 from langchain.embeddings import BedrockEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.vectorstores import FAISS
 from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-
+# Initialize boto3 clients
 s3 = boto3.client('s3')
-ssm_client = boto3.client('ssm')
 lambda_client = boto3.client('lambda')
+ssm_client = boto3.client('ssm')
 
 third_lambda_arn = os.getenv('CHARTMATE_EXTRACTION_FUNCTION_ARN')
-print("@",third_lambda_arn)
 
 def invoke_secondary_lambda_async(payload):
     response = lambda_client.invoke(
@@ -29,29 +24,30 @@ def invoke_secondary_lambda_async(payload):
     )
     return response
 
-bedrock_runtime = boto3.client( 
-        service_name="bedrock-runtime",
-        region_name="us-east-1",
-    )
+# Initialize BedrockEmbeddings and VectorstoreIndexCreator
+bedrock_runtime = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
 
 embeddings = BedrockEmbeddings(
-        model_id="amazon.titan-embed-text-v1",
-        client=bedrock_runtime,
-        region_name="us-east-1",
-    )
-
+    model_id="amazon.titan-embed-text-v1",
+    client=bedrock_runtime,
+    region_name="us-east-1",
+)
 
 index_creator = VectorstoreIndexCreator(
-        vectorstore_cls=FAISS,
-        embedding=embeddings,
-    )
+    vectorstore_cls=FAISS,
+    embedding=embeddings,
+)
 
 def lambda_handler(event, context):
-    print("1",event)
+    # Capture the start time from the event
     job_id = event['job_id']
     bucket_name = "chartmate-idp"
     response = s3.list_objects_v2(Bucket=bucket_name, Prefix=job_id)
+    print("response ",response)
+    temp_dir = '/tmp'
     txt_file_key = next((obj['Key'] for obj in response.get('Contents', []) if obj['Key'].endswith('.txt')), None)
+    print("2",txt_file_key)
+
 
     if txt_file_key:
         temp_dir = '/tmp'
@@ -60,40 +56,31 @@ def lambda_handler(event, context):
         print(f"File downloaded successfully: {temp_path}")
     else:
         print("No text files found in the folder.")
-
+    
     loader = TextLoader(temp_path)
     docs = loader.load()
     text_splitter = RecursiveCharacterTextSplitter()
     documents = text_splitter.split_documents(docs)
     vector = FAISS.from_documents(documents, embeddings)
     vector.save_local(folder_path="/tmp/", index_name='index')
-    # vector = FAISS.from_documents(documents, embeddings)
-
-    # index_from_loader = index_creator.from_loaders([loader])
-
-    # index_from_loader.vectorstore.save_local("/tmp")
 
     s3.upload_file(
         "/tmp/index.faiss", bucket_name, f"{job_id}/embeddings/index.faiss"
     )
     s3.upload_file("/tmp/index.pkl", bucket_name, f"{job_id}/embeddings/index.pkl")
-    
 
     payload = {
-        "job_id": job_id,
-        "event_data": event
+        "job_id": job_id
     }
-    
+
     ssm_client.put_parameter(
         Name=job_id,
         Value="Vector Generated",
         Type='String',
         Overwrite=True
     )
+
     invoke_secondary_lambda_async(payload)
-
-
-    # print(result)
 
     return {
         "statusCode": 200,
@@ -105,3 +92,4 @@ def lambda_handler(event, context):
         },
         "body": json.dumps("hello"),
     }
+
